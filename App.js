b17f24ae60fe867ecad66359978a893ea24dafae
Norman Henriquez <normanbetanco@gmail.com>
2023-06-04 19:09:53 -0600

    video 55

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StyleSheet,View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import {LinearGradient} from 'expo-linear-gradient'
 
 export default function App() {
-  return <View style={styles.rootScreen}><StartGameScreen/></View>;
+  return (
+    <LinearGradient colors={["#ddb52f","#4e0329"]} style={styles.rootScreen}>
+      <StartGameScreen/>
+
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
